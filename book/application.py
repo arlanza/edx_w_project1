495f67157c560c702eb45353e35707398a442211
Laura Alonso Ibáñez <laura@satec.es>
2020-04-12 00:19:34 +0200

    Login and Logout

--- book/application.py
+++ book/application.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask, render_template, request, session
 from flask_session import Session
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, or_, and_
 from sqlalchemy.orm import scoped_session, sessionmaker
 from model import *
 
@@ -16,6 +16,11 @@ else:
     app.config["SQLALCHEMY_DATABASE_URI"] ="postgres://xcnhprolmjsfye:4c46c46015f74d72337667afdbaaa0632951292cc55bbde2e7b0d124efd29e76@ec2-46-137-177-160.eu-west-1.compute.amazonaws.com:5432/devprdb0tctod0"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
+
+# To enable session
+app.secret_key = 'super secret key for book app'
+
+
 
 
 # Configure session to use filesystem
@@ -48,7 +53,7 @@ def registration():
         name     = request.form.get("name")
         pwd      = request.form.get("pwd")
     except ValueError:
-            return render_template("error.html", message="Not empty data allowed")
+            return render_template("error.html", message="Invalid data")
 
     if not fullname or not name or not pwd:
         return render_template("error.html", message="Not empty data allowed")
@@ -63,5 +68,53 @@ def registration():
     db.add(user)
     db.commit()
 
+    # Loggin in session
+    session['user_name']=user.name;
+
     #output
-    return render_template("success.html", message= f"{fullname.upper()} registrated.")
+    return render_template("success.html", message= f"{user.name} registrated.")
+
+@app.route('/signin')
+def signin():
+    return render_template("login.html")
+
+
+@app.route('/login', methods=["POST"])
+def login():
+    """Login a reader."""
+    # Get form information.
+    try:
+        name     = request.form.get("name")
+        passwd      = request.form.get("pwd")
+    except ValueError:
+            return render_template("error.html", message="Invalid data")
+
+    if not name or not passwd:
+        return render_template("error.html", message="Not empty data allowed")
+
+    # Not previous user registed
+    #user = User.query.filter(and_(name==name,passwd==passwd)).first()
+    #user =  User.query.get(name).filter_by(passwd=passwd)).first()
+    user = User.query.get(name)
+    if user is None:
+        return render_template("error.html", message="Try another Nick")
+
+    if not user.passwd == passwd:
+        return render_template("success.html", message= "Invalid passwd")
+
+    session['user_name']=user.name;
+
+    #if 'visits' in session:
+    #    session['visits'] = session.get('visits') + 1  # reading and updating session data
+    #else:
+    #    session['visits'] = 1 # setting session data
+    #return render_template("success.html", message= f"{session.get('visits'), {session.get(user.name)},}   num de visitas.")
+
+
+    return render_template("success.html", message= f"{session.get('user_name')} logged in")
+
+@app.route('/logout', methods=["GET","POST"])
+def logout():
+    name = session['user_name']
+    session.pop('user_name', None)
+    return render_template("success.html", message= f"{name} logged out")
--- book/model.py
+++ book/model.py
@@ -9,11 +9,26 @@ class Book(db.Model):
     author = db.Column(db.String, nullable=False)
     year = db.Column(db.Integer, nullable=False)
 
+    def print_info(self):
+        print(f"Isbn: {self.isbn}")
+        print(f"Title: {self.title}")
+        print(f"Author: {self.author}")
+        print(f"Year: {self.year}")
+
 class User(db.Model):
     __tablename__ = "usr"
     name = db.Column(db.String, primary_key=True)
     passwd = db.Column(db.String, nullable=False)
     fullname = db.Column(db.String, nullable=False)
+
+    def print_info(self):
+        print(f"Name: {self.name}")
+        print(f"Fullname: {self.fullname}")
+
+    def print_info_ext(self):
+        print(f"Name: {self.name}")
+        print(f"Passwd: {self.passwd}")
+        print(f"Fullname: {self.fullname}")
 
 class Review(db.Model):
     __tablename__ = "review"
@@ -21,3 +36,9 @@ class Review(db.Model):
     usr_name = db.Column(db.String, db.ForeignKey("usr.name"), primary_key=True)
     text = db.Column(db.String, nullable=False)
     rating = db.Column(db.Integer, nullable=False)
+
+    def print_info(self):
+        print(f"Book_isbn: {self.book_isbn}")
+        print(f"Usr_name: {self.usr_name}")
+        print(f"Text: {self.text}")
+        print(f"Rating: {self.rating}")
