e4c7bed179f2ef7bbc3beb36c5b0cb9bd2ed40aa
Laura Alonso Ibáñez <laura@satec.es>
2020-04-11 22:10:53 +0200

    Add registration

--- book/application.py
+++ book/application.py
@@ -1,26 +1,67 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, render_template, request, session
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from model import *
+
 
 app = Flask(__name__)
 
-# Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
+# Check for database_url environment variable and init sqlalchemy environmen
+if os.getenv("DATABASE_URL"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] ="postgres://xcnhprolmjsfye:4c46c46015f74d72337667afdbaaa0632951292cc55bbde2e7b0d124efd29e76@ec2-46-137-177-160.eu-west-1.compute.amazonaws.com:5432/devprdb0tctod0"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+
 
 # Configure session to use filesystem
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+#app.config["SESSION_PERMANENT"] = False
+#app.config["SESSION_TYPE"] = "filesystem"
+#Session(app)
 
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-
+#Index
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return render_template("index.html")
+
+
+#Show registration form
+@app.route("/signup")
+def signup():
+    return render_template("registration.html")
+
+#Registration
+@app.route("/registration", methods=["POST"])
+def registration():
+    """Register a reader."""
+    # Get form information.
+    try:
+        fullname = request.form.get("fullname")
+        name     = request.form.get("name")
+        pwd      = request.form.get("pwd")
+    except ValueError:
+            return render_template("error.html", message="Not empty data allowed")
+
+    if not fullname or not name or not pwd:
+        return render_template("error.html", message="Not empty data allowed")
+
+    # Not previous user registed
+    user = User.query.get(name);
+    if user is not None:
+        return render_template("error.html", message="There's already a user with this name. Choose another name, please.")
+
+    # Add User into DB
+    user = User(name=name, passwd=pwd, fullname=fullname)
+    db.add(user)
+    db.commit()
+
+    #output
+    return render_template("success.html", message= f"{fullname.upper()} registrated.")
