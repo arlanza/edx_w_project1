4619fc6298160669e7d0ddcd8f16bb4c7c4fcac7
Laura Alonso Ibáñez <laura@satec.es>
2020-04-11 08:57:33 +0200

    Test commit

--- env.sh
+++ env.sh
@@ -1,7 +1,8 @@
+#!/bin/csh
 set FLASK_APP=application.py
 set FLASK_DEBUG=1
 set DATABASE_URL=postgres://xcnhprolmjsfye:4c46c46015f74d72337667afdbaaa0632951292cc55bbde2e7b0d124efd29e76@ec2-46-137-177-160.eu-west-1.compute.amazonaws.com:5432/devprdb0tctod0
-
+#!/bin/bash
 FLASK_APP=application.py
 export FLASK_APP
 FLASK_DEBUG=1
